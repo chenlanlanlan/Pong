abe2bb333ac117811404d7feaf94b99a28211ffc
chenlanlanlan <chennylan@163.com>
2019-10-29 11:24:39 -0700

    initial commit

---
+++ Pong/Pong/Paddle.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+
+using namespace sf;
+
+class Paddle : public Shape
+{
+public:
+	Paddle();
+	~Paddle();
+
+private:
+	Vector2f paddleSize = Vector2f(25.f, 100.f);
+	float paddleX;
+	float paddleY;
+};
+
+Paddle::Paddle()
+{
+	RectangleShape paddle;
+	paddle.setSize(paddleSize - Vector2f(3, 3));
+	paddle.setOutlineThickness(3);
+	paddle.setOutlineColor(sf::Color::Black);
+	paddle.setFillColor(sf::Color(100, 100, 200));
+	paddle.setOrigin(paddleSize / 2.f);
+}
+
+Paddle::~Paddle()
+{
+}
---
+++ Pong/Pong/Pong.cpp
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include "Ball.cpp"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "GAME-230 Pong!", Style::Close);
+	window.setVerticalSyncEnabled(true);
+
+	Vector2f paddleSize = Vector2f(25.f, 100.f);
+	Ball ball;
+	sf::RectangleShape leftPaddle;
+	leftPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
+	leftPaddle.setOutlineThickness(3);
+	leftPaddle.setOutlineColor(sf::Color::Blue);
+	leftPaddle.setFillColor(sf::Color(100, 100, 200));
+	leftPaddle.setOrigin(paddleSize / 2.f);
+	leftPaddle.setPosition(20, 300);
+
+	sf::RectangleShape rightPaddle;
+	rightPaddle.setSize(paddleSize - sf::Vector2f(3, 3));
+	rightPaddle.setOutlineThickness(3);
+	rightPaddle.setOutlineColor(sf::Color::Green);
+	rightPaddle.setFillColor(sf::Color(200, 100, 100));
+	rightPaddle.setOrigin(paddleSize / 2.f);
+	rightPaddle.setPosition(780, 300);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.draw(leftPaddle);
+		//window.draw(ball);
+		window.draw(rightPaddle);
+		window.display();
+	}
+
+	return 0;
+}
